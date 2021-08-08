75c710369bf287d679bd2fb889f93ee3e901d655
unknown <msufyankhan709@gmail.com>
2021-08-08 13:09:42 -0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngIf';
+
+  disabledBox=true;
+  enableBox(){
+    this.disabledBox=false;
+  }
+  public show=true;
+  public show2=false;
+  public show3="no";
+
 }
